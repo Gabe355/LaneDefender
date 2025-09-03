b72f93c0ed2892e822ea6d43636e003cda834185
Gabe355 <gandrews1@mail.bradley.edu>
2025-09-03 14:47:13 -0500

    Enemy/Early lane addition

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    [SerializeField] private GameObject enemy1; 
+    [SerializeField] private GameObject enemy2;
+    [SerializeField] private GameObject enemy3;
+     private float spawnRate;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+    IEnumerator SpawnEnemy()
+    {       
+        while (true)
+        {
+            spawnRate = Random.Range(6, 12);
+            yield return new WaitForSeconds(spawnRate);
+            int chance = Random.Range(0, 5);
+            if (chance < 3)
+            {
+                Instantiate(enemy1, transform.position, Quaternion.identity);
+            }
+            else if (chance > 3)
+            {
+                Instantiate(enemy2, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(enemy3, transform.position, Quaternion.identity);
+            }            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
